4d4896096ee3081103d30936f9dc01860861fb38
kongmoumou <kongmoumou666@gmail.com>
2021-03-27 17:35:29 +0800

    chore: first commit

---
+++ src/test/java/com/jiucai/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiucai.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
