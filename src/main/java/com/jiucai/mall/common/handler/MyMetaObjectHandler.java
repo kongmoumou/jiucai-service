0fa4af693bde50431cd431542e401c37af8c09bf
kongmoumou <kongmoumou666@gmail.com>
2021-05-09 00:44:37 +0800

    fix: auto fill `create_time` & `update_time` field

---
+++ src/main/java/com/jiucai/mall/common/handler/MyMetaObjectHandler.java
@@ -0,0 +1,27 @@
+package com.jiucai.mall.common.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    /**
+     * 时间数据的自动填充
+     * @param metaObject
+     */
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐使用)
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now()); // 起始版本 3.3.0(推荐)
+    }
+
+}
