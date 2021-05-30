ef4ed1c4b301a14691bf29edbdb83e04f4dd3081
kun <1752735981@qq.com>
2021-05-31 01:05:09 +0800

    BASE64Decoder -> util.Base64

--- src/main/java/com/jiucai/mall/utils/OSSUtil.java
+++ src/main/java/com/jiucai/mall/utils/OSSUtil.java
@@ -10,7 +10,7 @@ import com.aliyun.oss.OSSException;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import sun.misc.BASE64Decoder;
+import java.util.Base64;
 
 @Component
 public class OSSUtil{
@@ -46,8 +46,8 @@ public class OSSUtil{
         long currentTime = System.currentTimeMillis();
         imageName = String.valueOf(currentTime) + "." + suffix;
 
-        BASE64Decoder decoder = new BASE64Decoder();
-        byte[] imageByte = decoder.decodeBuffer(image);
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] imageByte = decoder.decode(image);
 
         try {
             // 判断Bucket是否存在
